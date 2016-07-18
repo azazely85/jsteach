d069914d25172e6b8ad601fa0d5fc7729ff5fc14
Yaroslav Cherednyk <azazely85@gmail.com>
2016-07-18 16:18:04 +0300

    clg math

---
+++ clg.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Пользователь on 18.07.2016.
+ */
+function clg(data) {
+    console.log.apply(console,arguments);
+}
--- labmath.js
+++ labmath.js
@@ -1,3 +1,23 @@
 /**
  * Created by Пользователь on 18.07.2016.
  */
+
+var a= [5,12];
+var b = [];
+a[99]=7;
+for (var i=0;i<a.length;i++){
+    b.push(Math.pow(a[i],2));
+    //console.log(b[i]);
+}
+for (var i in a){
+    b.push(Math.pow(a[i],2));
+    //console.log(b[i]);
+}
+console.log(b);
+var a = [5, 45, 3, 100];
+var b = Math.min.apply(Math,a);
+console.log(b);
+//Math.min(5, 45, 3, 100);
+//Math.max(5, 45, 3, 100);
+// console.log(Math.PI);
+// console.log(Math.pow(4,2));//degree
