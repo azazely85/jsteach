72295a63ae1d062204df7415341183af849f2f59
Yaroslav Cherednyk <azazely85@gmail.com>
2016-07-18 16:58:37 +0300

    clg math fix2

--- labmath.js
+++ labmath.js
@@ -2,21 +2,38 @@
  * Created by Пользователь on 18.07.2016.
  */
 
-var a= [5,12];
-var b = [];
-a[99]=7;
-for (var i=0;i<a.length;i++){
-    b.push(Math.pow(a[i],2));
-    //console.log(b[i]);
+// var a= [5,12];
+// var b = [];
+// a[99]=7;
+// for (var i=0;i<a.length;i++){
+//     b.push(Math.pow(a[i],2));
+//     //console.log(b[i]);
+// }
+// for (var i in a){
+//     b.push(Math.pow(a[i],2));
+//     //console.log(b[i]);
+// }
+// console.log(b);
+// var a = [5, 45, 3, 100];
+// var b = Math.min.apply(Math,a);
+// clg(b);
+// (function () {
+//     clg(b);
+// })();//самовызывающая функция
+function foo() {
+    var a =[];
+        for(var i=0;i<3;i++){
+            (function (j) {
+            a.push(function (){
+                clg(j);
+            });})(i);
+        }
+    return a;
 }
-for (var i in a){
-    b.push(Math.pow(a[i],2));
-    //console.log(b[i]);
-}
-console.log(b);
-var a = [5, 45, 3, 100];
-var b = Math.min.apply(Math,a);
-clg(b);
+var x = foo();
+x[0]();
+x[1]();
+x[2]();
 //Math.min(5, 45, 3, 100);
 //Math.max(5, 45, 3, 100);
 // console.log(Math.PI);
